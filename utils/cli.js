07596c7598c8fc602306e887150f86d88328316d
meerhamzadev <hamzababar37@gmail.com>
2022-09-14 12:46:25 +0500

    📦 NEW: Add progress spinners

--- utils/cli.js
+++ utils/cli.js
@@ -1,4 +1,5 @@
 const exec = require('node-async-exec');
+const ora = require('ora');
 
 module.exports = async () => {
     try {
@@ -10,12 +11,15 @@ module.exports = async () => {
                 `touch .eslintrc.js`,
                 `echo "module.exports={ \'extends\' : \'airbnb-base\' }" > .eslintrc.js`
             ]
+        const spinner = ora('Installing dev dependencies').start();
         await exec({
             path: `${process.cwd()}/`,
             cmd: commands
         })
+        spinner.succeed(`Dependencies installed successfully.`);
+        spinner.succeed(`Successfully setup eslint and airbnb style guide.`);
     }
-    catch (e) {
-        console.log(`Error: ${e}`);
+    catch (error) {
+        return ora().start().fail(`unable to install the dependencies\n\n${error}`);
     }
 }
